345c062dd2b588db839e6977a4a5f00fae882e57
Frederick Javalera <frederickjavalera@gmail.com>
2021-09-28 20:26:02 -0600

    Initial Commit

---
+++ Shared/HelloCoreData_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreData_DemoApp.swift
+//  Shared
+//
+//  Created by Frederick Javalera on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreData_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
